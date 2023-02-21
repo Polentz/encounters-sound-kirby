3c6cfd44e216c35725aa3a013c3a4e2154226f35
Giulia Polenta <giuliapolenta@gmail.com>
2023-02-21 16:06:03 +0100

    Fix head tag and add favicons

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,43 +1,47 @@
+<?php
+    if ($kirby->language()->code() == 'it') {
+        $lang = 'it';
+        $langContent = 'it_IT';
+    } else if ($kirby->language()->code() == 'en') {
+        $lang = 'en';
+        $langContent = 'en_US';
+    }
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?= $lang ?>">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <?php if($page->isHomePage()) : ?>
         <title><?= $site->title() ?></title>
     <?php else : ?>
         <title><?= $site->title() ?> | <?= $page->title() ?></title>
     <?php endif ?>
-
     <meta name="description"
         content="<?= $site->description() ?>">
     <link rel="canonical" href="<?= $page->url() ?>">
     <meta name="keywords"
         content="<?= $site->keywords() ?>">
-    <meta property="og:locale" content="en_US">
+    <meta property="og:locale" content="<?= $langContent ?>">
     <meta property="og:type" content="website">
     <meta property="og:title" content="<?= $site->title()->inline()?>">
     <meta property="og:description"
         content="<?= $site->description() ?>">
     <meta property="og:url" content="<?= $page->url() ?>">
     <meta property="og:site_name" content="<?= $site->title()->inline()?>">
-    <!-- <meta property="og:image" content=""> -->
-    <!-- <meta property="og:image:alt" content=""> -->
-    <!-- <meta property="og:image" content=""> -->
+    <meta property="og:image" content="/assets/images/sound-objects-of-migrations.jpg">
+    <meta property="og:image" content="/assets/images/sound-objects-of-migrations.jpg">
     <meta name="twitter:card" content="summary">
     <meta name="twitter:description"
         content="<?= $site->description() ?>">
     <meta name="twitter:title"
         content="<?= $site->description() ?>">
-    <!-- <meta property="og:image" content=""> -->
-
-    <!-- <meta name="twitter:image:alt" content=""> -->
+    <meta property="og:image" content="/assets/images/sound-objects-of-migrations.jpg">
     <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
-    <!-- <link rel="shortcut icon" type="image/png" sizes="48x48" href="/assets/favicon/least-favicon-48.png"> -->
-    <!-- <link rel="shortcut icon" type="image/png" sizes="192x192" href="/assets/favicon/least-favicon-192.png"> -->
-
+    <link rel="shortcut icon" type="image/png" sizes="48x48" href="/assets/favicons/favicon-48.png">
+    <link rel="shortcut icon" type="image/png" sizes="192x192" href="/assets/favicons/favicon-192.png">
+    <link rel="shortcut icon" type="image/png" sizes="512x512" href="/assets/favicons/favicon-512.png">
     <?= css ([
         'assets/css/base.css',
         'assets/css/style.css',
