65bbd4fe7a86aa796a9dc6db65bc647ba1bab9f3
Giulia Polenta <giuliapolenta@gmail.com>
2023-03-03 11:23:00 +0100

    Change layout for text (deal with images)

--- site/snippets/info.php
+++ site/snippets/info.php
@@ -2,14 +2,23 @@
     <h3 class="info-title"><?= $info->infotitle()->inline() ?></h3>
     <div class="info-block">
         <div class="info-column">
-            <?= $info->columnone()->kt() ?>
-            <p class="info-caption"><?= $info->credits()->kt()->inline() ?></p>
+            <?php if ($info->hasimageleft()->isTrue()): ?>
+                <?php if ($imageleft = $info->imageleft()->toFile()) : ?>
+                    <img src="<?= $imageleft->crop(1200, 700, 72)->url() ?>" alt="<?= $imageleft->alt() ?>">
+                <?php endif ?>
+            <?php else : ?>
+                <?= $info->columnone()->kt() ?>
+                <p class="info-caption"><?= $info->creditsone()->kt()->inline() ?></p>
+            <?php endif ?>
         </div>
         <div class="info-column">
-            <?php if ($info->hasimage()->isTrue()): ?>
-                <?= $info->image() ?>
+            <?php if ($info->hasimageright()->isTrue()): ?>
+                <?php if ($imageright = $info->imageright()->toFile()) : ?>
+                    <img src="<?= $imageright->crop(1200, 700, 72)->url() ?>" alt="<?= $imageright->alt() ?>">
+                <?php endif ?>
             <?php else : ?>
                 <?= $info->columntwo()->kt() ?>
+                <p class="info-caption"><?= $info->creditstwo()->kt()->inline() ?></p>
             <?php endif ?>
         </div>
     </div>
